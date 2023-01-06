0f86056546b6e53104bcd1e1928643a6e1b0419e
baechanghyeon <pek5302@naver.com>
2023-01-06 21:54:05 +0900

    feat: Login Page 상태값 설정

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,34 @@
-import React from 'react';
+import React, { FormEvent, useState } from 'react';
 
 const Login = () => {
-  return <>Login</>;
+  const [input, setInput] = useState({
+    email: '',
+    password: '',
+  });
+  const { email, password } = input;
+
+  const onSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+  };
+
+  const onChange = (event: FormEvent<HTMLInputElement>) => {
+    const { name, value } = event.currentTarget;
+    setInput({
+      ...input,
+      [name]: value,
+    });
+  };
+
+  return (
+    <div>
+      <span>Login Page</span>
+      <form onSubmit={onSubmit}>
+        <input type='text' value={email} name='email' placeholder='Email 입력' onChange={onChange} />
+        <input type='text' value={password} name='password' placeholder='Password 입력' onChange={onChange} />
+        <input type='submit' value='로그인' />
+      </form>
+    </div>
+  );
 };
 
 export default Login;
