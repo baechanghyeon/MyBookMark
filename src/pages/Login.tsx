219254908609fa20566d966ff1405ed212bad43e
baechanghyeon <pek5302@naver.com>
2023-01-09 21:10:50 +0900

    fix: 로그인 폼 react-hook-form 라이브러리 사용

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,38 +1,57 @@
-import useForm from '@/hooks/useForm';
-import React, { useEffect, useState } from 'react';
-import validate from '@/utils/validate';
+import { useForm } from 'react-hook-form';
+import React from 'react';
 
-const Login = () => {
-  const { values, errors, submitting, handleChange, handleSubmit } = useForm({
-    initialValues: { email: '', password: '' },
-    onSubmit: (values) => {
-      alert(JSON.stringify(values, null, 2));
-    },
-    validate,
-  });
+function LoginForm({
+  onSubmit = async () => {
+    // login API 호출
+    await new Promise((r) => setTimeout(r, 1000));
+    alert('로그인 완료 !');
+  },
+}) {
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting, isDirty, errors },
+  } = useForm();
 
   const { email, password } = errors;
 
   return (
-    <div>
-      <span>Login Page</span>
-      <form onSubmit={handleSubmit}>
-        <input type='text' value={values.email} name='email' placeholder='Email 입력' onChange={handleChange} />
-        {errors ? email : null}
-        <br />
-        <input
-          type='password'
-          value={values.password}
-          name='password'
-          placeholder='Password 입력'
-          onChange={handleChange}
-        />
-        {errors ? password : null}
-        <br />
-        <input type='submit' value='로그인' disabled={submitting} />
-      </form>
-    </div>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <label htmlFor='email'>이메일</label>
+      <input
+        id='email'
+        type='text'
+        placeholder='test@email.com'
+        aria-invalid={!isDirty ? undefined : errors.email ? 'true' : 'false'}
+        {...register('email', {
+          required: '이메일은 필수 입력입니다.',
+          pattern: {
+            value: /\S+@\S+\.\S+/,
+            message: '이메일 형식에 맞지 않습니다.',
+          },
+        })}
+      />
+      {errors.email}
+      <label htmlFor='password'>비밀번호</label>
+      <input
+        id='password'
+        type='password'
+        placeholder='****************'
+        aria-invalid={!isDirty ? undefined : errors.password ? 'true' : 'false'}
+        {...register('password', {
+          required: '비밀번호는 필수 입력입니다.',
+          minLength: {
+            value: 8,
+            message: '8자리 이상 비밀번호를 사용하세요.',
+          },
+        })}
+      />
+      <button type='submit' disabled={isSubmitting}>
+        로그인
+      </button>
+    </form>
   );
-};
+}
 
-export default Login;
+export default LoginForm;
